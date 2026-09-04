d90dc77ec75882ab988fad27af8b609239f4fa0e
Justine Grégoire <justine.gregoire.1@icloud.com>
2026-09-04 15:31:07 -0400

    nombre_aleatoire_.py

---
+++ nombre_aleatoire_.py
@@ -0,0 +1,8 @@
+import random
+
+valeur_min = 50
+valeur_max = 100
+
+rnd = random.randint(a=valeur_min, b=valeur_max)
+
+print(f"Le nombre entier pige entre {valeur_min} et {valeur_max} est : \n{rnd}")
